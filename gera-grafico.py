aeabfad8aab34d26524154eb7c9ed0a3093d31dd
Rafael Passos <rafa_auyer@icloud.com>
2017-04-07 10:33:28 -0300

    Conversão de 'spaces' para 'tabs'

--- gera-grafico.py
+++ gera-grafico.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 def f(n):
         l=[]
         for i in n:
-		#INSIRA AQUI A FUNCAO f(n) representando-a por f(i)
-		l.append( np.math.factorial(i) )
+                #INSIRA AQUI A FUNCAO f(n) representando-a por f(i)
+                l.append(       np.math.factorial(i)    )
         return l
 
 def g(n):
         l=[]
         for i in n:
-		#INSIRA AQUI A FUNCAO g(n) representando-a por g(i)
-		l.append( i**i )
-	return l
+                #INSIRA AQUI A FUNCAO g(n) representando-a por g(i)
+                l.append( i**i )
+        return l
 
 #define intervalo e amostragem
-amostra = np.arange(0, 1000, 1)
+amostra = np.arange(0, 100, 1)
 
 plt.plot(amostra, f(amostra), 'b-',label="f(n)")
 plt.plot(amostra, g(amostra) , 'r--',label="g(n)")
 plt.xlabel("n")
 plt.ylabel("Passos")
-plt.title("Comportamento de Funções") 
+plt.title("Comportamento de Funções")
 
 
 lgd = plt.legend(loc='upper center', bbox_to_anchor=(0.5,0),fancybox=True, shadow=True)
